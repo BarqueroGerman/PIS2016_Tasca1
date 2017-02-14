054dac56c02b05e2f2e2f378f262a667ac77053d
barquerogerman <barquerogerman@gmail.com>
2017-02-14 10:13:07 +0100

    First co

---
+++ app/src/main/java/edu/ub/pis2016/gbarquero/pis2016_tasca1/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.ub.pis2016.gbarquero.pis2016_tasca1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
